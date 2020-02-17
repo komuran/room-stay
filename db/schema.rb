e01bc103b2ac507a4904d9dd5b9449f4f60afdda
KomuraHijiri <k16044kk@aitech.ac.jp>
2020-02-17 18:11:13 +0900

    Fix: 扱う値の変数名を修正

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2019_04_11_031247) do
 
   create_table "captures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "capture"
+    t.string "pcdev"
+    t.string "mac"
     t.integer "rssi"
-    t.integer "snr"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
